742335174ed467673d75e36f5ecc26689379bdbb
Will McCutchen <will@mccutch.org>
2014-10-22 18:19:12 -0400

    drop DOM-based layout code

--- slabtype.js
+++ slabtype.js
@@ -1,31 +1,4 @@
-var CONTAINER_CSS = [
-    'position: relative',
-    '-webkit-transform-origin: 50% 0',
-    '-moz-transform-origin: 50% 0',
-    '-o-transform-origin: 50% 0',
-    '-ms-transform-origin: 50% 0',
-    'transform-origin: 50% 0'
-].join(';');
-
-var LINE_CSS = [
-    'position:absolute',
-    'top: 0',
-    'left: 0',
-    'white-space: nowrap',
-    '-webkit-transform-origin: 0 0',
-    '-moz-transform-origin: 0 0',
-    '-o-transform-origin: 0 0',
-    '-ms-transform-origin: 0 0',
-    'transform-origin: 0 0'
-].join(';');
-
-function setVendorStyle(el, rule, value) {
-    el.style['-webkit-' + rule] = value;
-    el.style['-moz-' + rule] = value;
-    el.style['-o-' + rule] = value;
-    el.style['-ms-' + rule] = value;
-    el.style[rule] = value;
-}
+(function(globals) {
 
 function makeLines(text, targetLineLength) {
     // Use Erik Loyer's slabtype algorithm to split our input text into
@@ -33,7 +6,7 @@ function makeLines(text, targetLineLength) {
     // ideal line length to be given instead of automagically calculated.
     //
     // http://erikloyer.com/index.php/blog/the_slabtype_algorithm_part_1_background/
-    var words = text.split(/\s+/);
+    var words = text.trim().split(/\s+/);
 
     var lines = [];
     var preText, postText, lineText;
@@ -65,75 +38,6 @@ function makeLines(text, targetLineLength) {
         lines.push(lineText);
     }
     return lines;
-}
-
-function layout(el, targetLineLength, width, height) {
-    if (width === undefined) {
-        width = el.clientWidth;
-    }
-    if (height === undefined) {
-        height = el.clientHeight;
-    }
-
-    var text = el.innerText || el.textContent;
-    var lines = makeLines(text, targetLineLength);
-
-    // Add our lines to the DOM, where each line is wrapped in a <span> and all
-    // of the spans are wrapped in a <div>.
-    var spans = [];
-    var line;
-    for (var i = 0; i < lines.length; i++) {
-        line = lines[i];
-        spans.push(
-            '<span style="' + LINE_CSS + '">' +
-            line +
-            '</span>'
-        );
-    }
-    el.innerHTML = (
-        '<div class="slabtext" style="' + CONTAINER_CSS + '">' +
-        spans.join('\n') +
-        '</div>'
-    );
-
-    // Lay out the lines optimally within the given bounds.
-    var lineEls = el.querySelectorAll('span');
-    var lineCount = lineEls.length;
-    var lineEl;
-    var scale;
-    var rect;
-    var totalHeight = 0;
-    for (i = 0; i < lineCount; i++) {
-        lineEl = lineEls[i];
-        scale = width / lineEl.offsetWidth;
-        if (scale !== 1) {
-            setVendorStyle(lineEl, 'transform', 'scale(' + scale + ',' + scale + ')');
-            lineEl.style['top'] = totalHeight + 'px';
-            rect = lineEl.getBoundingClientRect();
-            totalHeight += rect.height;
-        }
-    }
-
-    var wrapper = el.querySelector('.slabtext');
-    var containerScale = 1;
-    if (totalHeight <= height) {
-        // Our text fits, so center it vertically
-        var offset = (height - totalHeight) / 2;
-        setVendorStyle(wrapper, 'transform', 'translateY(' + offset + 'px)');
-    } else {
-        // Our text is too tall, so scale the whole container down and center
-        // it horizontally.
-        containerScale = height / totalHeight;
-        setVendorStyle(wrapper, 'transform',
-            'scale(' + containerScale + ',' + containerScale + ')');
-    }
-
-    return {
-        'slabHeight': totalHeight * containerScale,
-        'containerHeight': height,
-        'containerWidth': width,
-        'scale': containerScale
-    };
 }
 
 function getLineHeight(computedStyle) {
@@ -188,7 +92,7 @@ function prepareContext(canvasEl, computedStyle) {
     return prepareContextShadow(ctx, computedStyle);
 }
 
-function layoutCanvas(el, targetLineLength, width, height) {
+function layout(el, targetLineLength, width, height) {
     if (width === undefined) {
         width = el.clientWidth;
     }
@@ -211,6 +115,8 @@ function layoutCanvas(el, targetLineLength, width, height) {
     var lineHeight = getLineHeight(computedStyle);
 
     if (canvasEl !== el) {
+        canvasEl.style['height'] = height + 'px';
+        canvasEl.style['width'] = width + 'px';
         el.parentNode.replaceChild(canvasEl, el);
         el = null;
     }
@@ -282,9 +188,8 @@ function layoutCanvas(el, targetLineLength, width, height) {
     };
 }
 
-var Slabtype = {
-    'layout': layout,
-    'layoutCanvas': layoutCanvas
+globals['Slabtype'] = {
+    'layout': layout
 };
 
-Slabtype = Slabtype;
+})(window);
